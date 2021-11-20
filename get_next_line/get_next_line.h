2880b2297a6922a7bdf9015714e1b6468992e0a8
Tanguy Pauvret <tpauvret@c1r12s1.42lausanne.ch>
2021-11-20 14:10:48 +0100

    adding gnl for parsing map

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpauvret <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 17:30:48 by tpauvret          #+#    #+#             */
+/*   Updated: 2021/10/23 19:51:24 by tpauvret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*get_next_line(int fd);
+char		*ft_strdup(const char *s1);
+size_t		ft_strlen(const char *str);
+
+#endif
